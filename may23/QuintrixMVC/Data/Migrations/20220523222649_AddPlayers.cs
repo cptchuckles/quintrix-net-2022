5c21884100acc02638687ef2243dc1d73576bf9f
Grendel <jcg@grendel.world>
2022-05-23 18:23:34 -0500

    Add MVC app with Players table

---
+++ may23/QuintrixMVC/Data/Migrations/20220523222649_AddPlayers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuintrixMVC.Data.Migrations
+{
+    public partial class AddPlayers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                    name: "Players",
+                    columns: table => new {
+                        Id = table.Column<Guid>(nullable: false),
+                        Name = table.Column<string>(nullable: false),
+                        Email = table.Column<string>(nullable: false)
+                    },
+                    constraints: table => {
+                        table.PrimaryKey("PK_Player", item => item.Id);
+                    });
+
+            //Operation not supported by SQlite3:
+            //migrationBuilder.AddPrimaryKey(name: "PK_Player", table: "Players", column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Players");
+        }
+    }
+}
---
+++ may23/QuintrixMVC/Models/Player.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text.RegularExpressions;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayerModel
+{
+	public class Player
+	{
+		private static Regex _validEmailPattern = new Regex(
+				@"^[a-zA-Z]\w*@(\w+\.)+\w+$",
+				RegexOptions.Compiled
+				);
+
+		[Key]
+		public Guid Id { get; set; }
+
+		private string _name;
+		public string Name { get => _name; set => _name = value; }
+
+		private string _email;
+		public string Email
+		{
+			get => _email;
+			set => _email = ValidateEmail(value);
+		}
+
+		public Player(string name, string email)
+		{
+			Name = name;
+			Email = email;
+		}
+
+		private static string ValidateEmail(string input)
+		{
+			if (! _validEmailPattern.IsMatch(input))
+				throw new Exception("Invalid email given");
+
+			return input;
+		}
+	}
+}

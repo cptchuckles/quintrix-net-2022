b37ec686ace7082613a91d8117ee8c6788b9028d
Grendel <jcg@grendel.world>
2022-05-24 17:52:24 -0500

    Add constraints to Robot properties

--- may23/QuintrixMVC/Models/Robot.cs
+++ may23/QuintrixMVC/Models/Robot.cs
@@ -8,8 +8,24 @@ namespace QuintrixMVC.Models
         [Key]
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value.ToUpper().Replace(' ', '_');
+        }
 
-        public uint PowerLevel { get; set; }
+        private uint _powerLevel;
+        public uint PowerLevel
+        {
+            get => _powerLevel;
+            set
+            {
+                if (value < 9000)
+                    throw new Exception("Your power level is too MEASLEY");
+
+                _powerLevel = value;
+            }
+        }
     }
 }
